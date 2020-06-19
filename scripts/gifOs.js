f2b6816833068474b3699c5aaba17d5bf8ded632
kitzi66 <kitzi66@gmail.com>
2020-06-18 21:05:29 -0500

    correccion ubicacion de las imagenes

--- scripts/gifOs.js
+++ scripts/gifOs.js
@@ -201,7 +201,7 @@ function fillGalery(id_seccion, title, data, numero_imagenes) {
                     spanTitulo.innerHTML = datos_imagen.title;
                     divTitulo.appendChild(spanTitulo);
                     let imgTitulo = document.createElement('img');
-                    imgTitulo.src = '/gifOs/imagenes/button3.svg';
+                    imgTitulo.src = '../imagenes/button3.svg';
                     divTitulo.appendChild(imgTitulo);
                     articulo.appendChild(divTitulo);
 
